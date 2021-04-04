3999da1647a22e21e46930f8016804dadd2f61fd
Audrey Lace <audrey.lace.taylor@gmail.com>
2021-04-04 10:33:14 -0500

    Ignore code field in results error details

--- Messages/Incoming/ResultMessageErrorDetails.cs
+++ Messages/Incoming/ResultMessageErrorDetails.cs
@@ -4,6 +4,8 @@ namespace AudreysCloud.Community.SharpHomeAssistant.Messages
 {
 	public class ResultMessageErrorDetails
 	{
+		//TODO - Code is broken, a string is getting sent back, ignore for now
+		[JsonIgnore]
 		[JsonPropertyName("code")]
 		public int Code { get; set; }
 
